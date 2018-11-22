b227a98219ad77de5322521ff82c506530f55412
Gerivan Santos <gerivantpc3@gmail.com>
2018-11-22 21:15:11 -0200

    Projeto Criado

---
+++ src/main/java/com/gerivansantos/cursomc/CussomcApplication.java
@@ -0,0 +1,12 @@
+package com.gerivansantos.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CussomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CussomcApplication.class, args);
+	}
+}
