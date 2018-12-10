a261a514325ed90889da973712cff8645dd94310
Junior <gerivan.c.junior@accenture.com>
2018-12-10 14:16:47 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/gerivansantos/cursomc/CursomcApplication.java
+++ src/main/java/com/gerivansantos/cursomc/CursomcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CussomcApplication {
+public class CursomcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CussomcApplication.class, args);
+		SpringApplication.run(CursomcApplication.class, args);
 	}
 	
 	
--- src/test/java/com/gerivansantos/cursomc/CursomcApplicationTests.java
+++ src/test/java/com/gerivansantos/cursomc/CursomcApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CussomcApplicationTests {
+public class CursomcApplicationTests {
 
 	@Test
 	public void contextLoads() {
